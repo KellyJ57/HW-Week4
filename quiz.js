29961145aa6436525c6c0110f13c1128acb2af37
Kelly <jenkins0703@hotmail.com>
2019-12-11 12:56:31 -0700

    I give up

--- quiz.js
+++ quiz.js
@@ -1,58 +1,162 @@
-const startButton = document.getElementById("start-btn")
-const nextButton = document.getElementById("next-btn")
-const questionContainerElement = document.getElementById("question-container")
-let shuffledQuestions, currentQuestionIndex
-const questionElement = document.getElementById("question")
-const answerButtons = document.getElementById("answer-buttons")
+var QuestionIndex = 0;
+var time = questions.length * 15;
+var timerId;
+
+var questions = document.getElementById("questions");
+var time = document.getElementById("timer");
+var choices = document.getElementById("choices");
+var submitBtn = document.getElementById("submit");
+var startBtn = document.getElementById("start");
+var initialsEl = document.getElementById("initials");
+var feedbackEl = document.getElementById("feedback");
+
+function startQuiz() {
+
+	var startScreenEl = document.getElementById("start-screen");
+	startScreenEl.setAttribute("class", "hide");
 
 
-startButton.addEventListener("click", startGame)
+	questions.removeAttribute("class");
 
-function startGame() {
-	startButton.classList.add("hide");
-	shuffledQuestions = questions.sort(() => Math.random() - .5);
-	currentQuestionIndex = 0;
-	questionContainerElement.classList.remove("hide");
-	setNextQuestion()
+
+	timerId = setInterval(clockTick, 1000);
+
+
+	time.textContent = time;
+
 }
 
-function setNextQuestion() {
-	resetState();
-	showQuestion(shuffledQuestions[currentQuestionIndex]);
+function getQuestion() {
+
+	var currentQuestion = questions[QuestionIndex];
+
+
+	var title = document.getElementById("question-title");
+	title.textContent = currentQuestion.title;
+
+
+	choicesEl.innerHTML = "";
+
+
+	currentQuestion.choices.forEach(function (choice, i) {
+
+		var choiceNode = document.createElement("button");
+		choiceNode.setAttribute("class", "choice");
+		choiceNode.setAttribute("value", choice);
+
+		choiceNode.textContent = i + 1 + ". " + chosice;
+
+
+		choiceNode.onclick = questionClick;
+
+		choicesEl.appendChild(choiceNode);
+	});
 }
 
-function showQuestion(question){
-    questionElement.innerText = question.question;
-    question.answers.forEach(answer => {
-    	const button = document.createElement("button");
-    	button.innerText = answer.text;
-    	button.classList.add("btn");
-    	if (answer.correct) {
-    		button.dataset.correct = answer.correct
-    	}
-    	button.addEventListener("click", selectAnswer)
-    	answerButtons.appendChild(button);
-    })
-}
+function questionClick() {
+	
+	if (this.value !== questions[currentQuestionIndex].answer) {
+		
+		time -= 15;
 
-function resetState() {
-	nextButton.classList.add("hide");
-	while (answerButtons.firstChild) {
-		answerButtons.removeChild;
-		(answerButtons.firstChild);
+		if (time < 0) {
+			time = 0;
+		}
+
+		
+		timerEl.textContent = time;
+
+		
+		sfxWrong.play();
+
+		feedbackEl.textContent = "Wrong!";
+	} else {
+		
+		sfxRight.play();
+
+		feedbackEl.textContent = "Correct!";
+	}
+
+	
+	feedbackEl.setAttribute("class", "feedback");
+	setTimeout(function () {
+		feedbackEl.setAttribute("class", "feedback hide");
+	}, 500);
+
+	
+	currentQuestionIndex++;
+
+
+	if (currentQuestionIndex === questions.length) {
+		quizEnd();
+	} else {
+		getQuestion();
 	}
 }
 
-function selectAnswer(e) {
+function quizEnd() {
+	
+	clearInterval(timerId);
 
+	
+	var endScreenEl = document.getElementById("end-screen");
+	endScreenEl.removeAttribute("class");
+
+	
+	var finalScoreEl = document.getElementById("final-score");
+	finalScoreEl.textContent = time;
+
+	
+	questionsEl.setAttribute("class", "hide");
 }
 
-const questions = [
-{
-	question: "Which is the smallest planet in our solar system?",
-	answers: [
-	       {text:"jupiter", correct:true},
-	       {text:"mercury", correct:false}
-	]
+function clockTick() {
+
+	time--;
+	timerEl.textContent = time;
+
+	
+	if (time <= 0) {
+		quizEnd();
+	}
 }
-]
+
+function saveHighscore() {
+	
+	var initials = initialsEl.value.trim();
+
+	
+	if (initials !== "") {
+		
+		var highscores =
+			JSON.parse(window.localStorage.getItem("highscores")) || [];
+
+		
+		var newScore = {
+			score: time,
+			initials: initials
+		};
+
+		
+		highscores.push(newScore);
+		window.localStorage.setItem("highscores", JSON.stringify(highscores));
+
+		
+		window.location.href = "highscores.html";
+	}
+}
+
+function checkForEnter(event) {
+	
+	if (event.key === "Enter") {
+		saveHighscore();
+	}
+}
+
+
+submitBtn.onclick = saveHighscore;
+
+
+startBtn.onclick = startQuiz;
+
+initialsEl.onkeyup = checkForEnter;
